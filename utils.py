ec67096ff33843a58a3bea697e809ac4875ddd93
Alexander Galazin <alexander.galazin@arm.com>
2021-04-01 13:53:40 +0300

    Update withdrawn releases

--- utils.py
+++ utils.py
@@ -48,7 +48,9 @@ WITHDRAWN_RELEASES	= ['vulkan-cts-1\.0\.0\.[0-9]*',
 					   'vulkan-cts-1\.1\.1\.[0-9]*',
 					   'vulkan-cts-1\.1\.2\.[0-9]*',
 					   'vulkan-cts-1\.1\.3\.[0-9]*',
-					   'vulkan-cts-1\.1\.4\.[0-9]*']
+					   'vulkan-cts-1\.1\.4\.[0-9]*',
+					   'vulkan-cts-1\.1\.5\.[0-9]*',
+					   'vulkan-cts-1\.2\.0\.[0-9]*']
 NOT_MASTER_DIR		= ['vulkan-cts-1\.0\.[0-9]*\.[0-9]*',
 					   'vulkan-cts-1\.1\.0\.[0-9]*',
 					   'vulkan-cts-1\.1\.1\.[0-9]*',
