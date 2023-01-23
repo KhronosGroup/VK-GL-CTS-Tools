8bbead62c8b1421e15cde52b4104bd300ed408ba
Matthew Netsch <quic_mnetsch@quicinc.com>
2023-01-23 08:04:25 -0800

    Withdraw Vulkan CTS 1.2.7

--- utils.py
+++ utils.py
@@ -60,7 +60,8 @@ WITHDRAWN_RELEASES	= ['vulkan-cts-1\.0\.0\.[0-9]*',
 				'vulkan-cts-1\.2\.3\.[0-9]*',
 				'vulkan-cts-1\.2\.4\.[0-9]*',
 				'vulkan-cts-1\.2\.5\.[0-9]*',
-				'vulkan-cts-1\.2\.6\.[0-9]*']
+				'vulkan-cts-1\.2\.6\.[0-9]*',
+				'vulkan-cts-1\.2\.7\.[0-9]*']
 NOT_MASTER_DIR		= ['vulkan-cts-1\.0\.[0-9]*\.[0-9]*',
 					   'vulkan-cts-1\.1\.0\.[0-9]*',
 					   'vulkan-cts-1\.1\.1\.[0-9]*',
