74fb5565a2e38800aecfb2cf473e6e74b0850319
Lorenzo Dal Col <lordalcol@users.noreply.github.com>
2026-06-12 11:09:09 +0200

    Withdrawal vulkan-cts-1.4.1.x

--- utils.py
+++ utils.py
@@ -75,6 +75,7 @@ WITHDRAWN_RELEASES	= [r'vulkan-cts-1\.0\.0\.[0-9]*',
 				r'vulkan-cts-1\.3\.9\.[0-9]*',
 				r'vulkan-cts-1\.3\.10\.[0-9]*',
 				r'vulkan-cts-1\.4\.0\.[0-9]*',
+				r'vulkan-cts-1\.4\.1\.[0-9]*',
 				r'vulkansc-cts-1\.0\.0\.[0-9]*',
 				r'vulkansc-cts-1\.0\.1\.[0-9]*']
 NOT_MASTER_DIR		= [r'vulkan-cts-1\.0\.[0-9]*\.[0-9]*',
